6e8d15a2e48b13db611b7ef295bd1159bfa4bab2
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-17 16:33:54 +0530

    api

--- api1/src/App.js
+++ api1/src/App.js
@@ -1,6 +1,27 @@
 import React,{Component} from 'react';
 
 class App extends Component{
+
+    constructor(props){
+      super(props);
+
+      this.state={
+        items:[],
+        isLoaded:false
+      }
+    }
+
+      componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>json())
+        .then(json=>{
+          this.setState({
+            isLoaded:true,
+            items:json
+          })
+        });
+      }    
+
   render(){
     return(
       <div className="App"> 
